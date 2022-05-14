4e6268973b7e03e42206737fa776f9962e12b902
Alan Seo <alan@grepp.co>
2022-05-15 04:12:05 +0900

    Add problem 2.2

--- chapter2/digit_conversion/src/main.rs
+++ chapter2/digit_conversion/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     problem2_1();
+    println!("-------------");
+    problem2_2();
 }
 
-
-fn problem2_1 () {
+fn problem2_1() {
     let a = 0x25B9D2;
     let b = 0b1010111001001001;
     let c = 0xA8B3D;
@@ -13,4 +14,24 @@ fn problem2_1 () {
     println!("{:x}", b);
     println!("{:b}", c);
     println!("{:x}", d);
+}
+
+fn problem2_2() {
+    let digits: [u32; 7] = [
+        5,
+        23,
+        get_n_from_pow(32768),
+        get_n_from_pow(0x2000),
+        12,
+        get_n_from_pow(64),
+        get_n_from_pow(0x100),
+    ];
+
+    for digit in digits.into_iter() {
+        println!("n = {0}, 2^n = {1}, hex of 2^n = {1:x}", digit, 2i32.pow(digit))
+    }
+}
+
+fn get_n_from_pow(n_pow: i32) -> u32 {
+    (n_pow as f64).log2() as u32
 }
