d5bacc7b2632b6ce2094a70808d7e1c82896140f
Alan Seo <alan@grepp.co>
2022-05-14 22:42:00 +0900

    Add chapter2

---
+++ chapter2/digit_conversion/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    problem2_1();
+}
+
+
+fn problem2_1 () {
+    let a = 0x25B9D2;
+    let b = 0b1010111001001001;
+    let c = 0xA8B3D;
+    let d = 0b1100100010110110010110;
+
+    println!("{:b}", a);
+    println!("{:x}", b);
+    println!("{:b}", c);
+    println!("{:x}", d);
+}
