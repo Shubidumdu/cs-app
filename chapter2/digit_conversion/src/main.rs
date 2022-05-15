64466ebc84f8b7590b3eb75b5c564c30515fbed2
Alan Seo <alan@grepp.co>
2022-05-15 18:31:23 +0900

    Add problem2_3

--- chapter2/digit_conversion/src/main.rs
+++ chapter2/digit_conversion/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     problem2_1();
     println!("-------------");
     problem2_2();
+    println!("-------------");
+    problem2_3();
 }
 
 fn problem2_1() {
@@ -34,4 +36,11 @@ fn problem2_2() {
 
 fn get_n_from_pow(n_pow: i32) -> u32 {
     (n_pow as f64).log2() as u32
+}
+
+fn problem2_3() {
+    let digits: [u32; 10] = [0, 158, 76, 145, 0b10101110, 0b00111100, 0b11110001, 0x75, 0xBD, 0xF5];
+    for digit in digits.into_iter() {
+        println!("Decimal = {0}, Binary = {0:08b}, Hexadecimal = 0x{0:02x}", digit);
+    }
 }
