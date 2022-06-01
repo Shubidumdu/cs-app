b2dba18aa26603612b9f05892ad55769e4abaf86
Alan Seo <alan@grepp.co>
2022-06-01 15:51:36 +0900

    Add reverse_array in bit_operation

--- chapter2/bit_operation/src/main.rs
+++ chapter2/bit_operation/src/main.rs
@@ -1,4 +1,7 @@
 fn inplace_swap(x: *mut i32, y: *mut i32) {
+    if x == y {
+        return;
+    }
     unsafe {
         println!("{:?}, {:?}", x.read(), y.read());
         *y ^= *x;
@@ -8,8 +11,25 @@ fn inplace_swap(x: *mut i32, y: *mut i32) {
     }
 }
 
+fn reverse_array(mut a: Vec<i32>, cnt: usize) {
+    let mut first: usize = 0;
+    let mut last: usize = cnt - 1;
+    
+    while first <= last {
+        let first_ptr: *mut i32 = &mut a[first];
+        let last_ptr: *mut i32 = &mut a[last];
+        inplace_swap(first_ptr, last_ptr);
+        first += 1;
+        last -= 1;
+    }
+
+    println!("{:?}", a);
+}
+
 fn main() {
-    let x: *mut i32 = &mut 2;
-    let y: *mut i32 = &mut 4;
-    inplace_swap(x, y);
+    // let x: *mut i32 = &mut 2;
+    // let y: *mut i32 = &mut 4;
+    // inplace_swap(x, y);
+    let v = vec![1, 2, 3, 4, 5];
+    reverse_array(v, 5);
 }
